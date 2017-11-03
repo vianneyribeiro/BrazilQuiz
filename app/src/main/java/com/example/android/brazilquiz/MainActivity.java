0901031fdb833c7a53b0f186ee4ceb1be884df14
vianneyribeiro <vianneyribeiro@gmail.com>
2017-11-02 22:57:53 -0300

    Correção do código Main, Acrescenta checkagem na proposição das bandeiras e dos rios para verificar se alguma resposta incorreta tambem foi clicada Mudança nas respostas

--- app/src/main/java/com/example/android/brazilquiz/MainActivity.java
+++ app/src/main/java/com/example/android/brazilquiz/MainActivity.java
@@ -11,13 +11,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public int verificaRespostas() {
